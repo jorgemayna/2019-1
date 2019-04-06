ac7f083952cd5000b5580030a95af5efb0444ab0
jorge <jorgemayna>
2019-04-05 23:59:21 -0500

    ploteo

--- algoritmos/p3.cpp
+++ algoritmos/p3.cpp
@@ -1,16 +1,12 @@
 #include<iostream>
 #include<ctime>
 #include<unistd.h>
+#include<fstream>
 //#include<windows.h>
 using namespace std;
 #define INF  999999999
 
-/*double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b){
-  LARGE_INTEGER freq;
-  QueryPerformanceFrequency(&freq);
-  return (double)(a->QuadPart - b->QuadPart) / (double)freq.QuadPart;
-}
-*/
+
 
 void print_arr(int* _array,int n){
     for(int i=0;i<n;i++){
@@ -66,71 +62,66 @@ void merge_sort(int* _array,int i,int d){
 }
 
 int main(){
-    int n;
+    int n,total=20000;
     int * _array;
-    int * _array1;
-    unsigned t0,t1;
-    //LARGE_INTEGER t_ini, t_fin;
-    //LARGE_INTEGER t_ini1, t_fin1;
-    double secs,secs1;
+    ofstream file;
+    file.open("record.txt");
 
     cout<<"Ingrese tamaño: ";
     cin>>n;
-    _array = new int[n];
-    _array1 = new int[n];
+
     cout<<endl;
+    for(int num=1;num<=n;num++){
+    unsigned t0,t1;
+    double co1=0;
+    for(int zz=0;zz<total;zz++){
+        _array = new int[num];
+        for(int i=0;i<num;i++){
+            _array[i]=num-i;
+        }
+        //cout<<"INSERTION SORT:------------------------------"<<endl;
+        //print_arr(_array,n);
 
-    double co1=0,co2=0;
-    for(int zz=0;zz<100000;zz++){
-    for(int i=0;i<n;i++){
-        _array[i]=n-i;
-        _array1[i]=n-i;
+        t0=clock();
+        usleep(200);
+        if(zz<total/2){
+            insertion_sort(_array,num);
+        }else{
+            merge_sort(_array,0,num);
+        }
+        t1=clock();
+
+        //print_arr(_array,n);
+        double time=(double(t1-t0)/CLOCKS_PER_SEC)*1000;
+        //cout<<"Tiempo de ejecucion: "<<time<<endl;
+        co1=co1+time;
+
+
+        //cout<<"MERGE SORT:----------------------------------"<<endl;
+
+        //print_arr(_array1,n);
+
+        //insertion_sort(_array1,n);
+
+        //print_arr(_array1,n);
+
+
+        //cout<<"---------------------------------------------"<<endl;
+        delete [] _array;
+        if(zz==total/2-1){
+            cout<<"insertion sort de "<<num<<": "<<co1/(total/2)<<endl;
+            file<<num<<"   "<<co1/(total/2)<< "   ";
+            co1=0;
+        }
     }
-    //cout<<"INSERTION SORT:------------------------------"<<endl;
-    //print_arr(_array,n);
 
-    //QueryPerformanceCounter(&t_ini);
-    t0=clock();
-    usleep(200);
-    insertion_sort(_array,n);
-    t1=clock();
-    //QueryPerformanceCounter(&t_fin);
+    cout<<"merge sort de "<<num<<": "<<co1/(total/2)<<endl<<endl;
+    file<<co1/(total/2)<<endl;
 
-    //print_arr(_array,n);
-    double time=(double(t1-t0)/CLOCKS_PER_SEC)*1000;
-    //cout<<"Tiempo de ejecucion: "<<time<<endl;
-    co1=co1+time;
-    //secs = performancecounter_diff(&t_fin, &t_ini);
-    //printf("%.16g millisegundos\n", secs * 1000.0);
 
-    //cout<<"MERGE SORT:----------------------------------"<<endl;
-
-    //print_arr(_array1,n);
-
-    //QueryPerformanceCounter(&t_ini1);
-
-    //insertion_sort(_array1,n);
-    //QueryPerformanceCounter(&t_fin1);
-
-    //print_arr(_array1,n);
-
-    //secs1 = performancecounter_diff(&t_fin1, &t_ini1);
-    //printf("%.16g millisegundos\n", secs1 * 1000.0);
-
-    //cout<<"---------------------------------------------"<<endl;
-    /*if(secs1<secs){
-        //cout<<"Merge sort gana"<<endl;
-        co1++;
-    }else{
-        //cout<<"Insert sort gana"<<endl;
-        co2++;
-    }*/
     }
-    cout<<"tiempo promedio: "<<co1/100000<<endl;
 
 
 
-    delete [] _array;
-    delete [] _array1;
 
 }
