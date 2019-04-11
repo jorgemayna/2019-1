e1e6c929c75ebcca3d545be7d85d374ae2caa035
jorgemayna <jorgemayna@gmail.com>
2019-04-11 14:01:37 -0500

    threads

---
+++ Sistemas operativos/semana_03/paralelo.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+void *printHola(void *num_hilo){
+	long t;
+	t=(long)num_hilo;
+	printf("Hola Mundo desde el hilo %ld \n",t);	
+	pthread_exit(NULL);
+}
+int main(){
+	int i,j,k,rc;
+	long t;
+	pthread_t hilo0,hilo1;
+	printf("Bienvenidos a hola \n");
+	t=0;
+	rc=pthread_create(&hilo0,NULL,printHola,(void*)t);
+	if(rc){
+		printf("erroe");
+		exit(-1);
+	}
+	t=1;
+	rc=pthread_create(&hilo1,NULL,printHola,(void*)t);
+	if(rc){
+		printf("erroe");
+		exit(-1);
+	}
+	pthread_exit(NULL);
+	return 0;
+}
