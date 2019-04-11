fd5b7af2f749f6998d3744f7bb30634dc8af1cac
jorgemayna <jorgemayna@gmail.com>
2019-04-10 23:08:58 -0500

    binary_search i and r

---
+++ algoritmos/semana 03/binary_search_i.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int binary_search_gg(int* arr,int n,int co){
+        int l=0;
+        int r=n-1;
+        while(r>=l){
+            int m=(l+r)/2;
+            int re=arr[m];
+            if(re<co){
+                l=m+1;
+            }else if(re>co){
+                r=m-1;
+            }else{
+                return m;
+            }
+        }
+    }
+int main(){
+    int n,num;
+    cin>>n;
+    int* arr;
+    arr=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=i+1;
+    }
+    cin>>num;
+    cout<<binary_search_gg(arr,n,num)<<endl;
+
+}
