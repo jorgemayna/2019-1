717af2d81ec7fd5060e4a0508d91f9810cf0ccad
utec <jorgemayna@gmail.com>
2019-04-25 12:49:26 -0500

    OS lab_3.1 casi acabado

--- Sistemas operativos/lab_3.1/filosofos.c
+++ Sistemas operativos/lab_3.1/filosofos.c
@@ -10,6 +10,7 @@ pthread_mutex_t lock2;
 pthread_mutex_t lock3;
 pthread_mutex_t lock4;
 pthread_mutex_t lock5;
+pthread_barrier_t our_barrier;
 
 pthread_t arreglo_hilos[NUM_HILOS];
 void* filosofo(void *id){
@@ -19,71 +20,78 @@ void* filosofo(void *id){
     int palitos=0;
     int a1=0,b1=0,c1=0,d1=0,e1=0;
     while(arr<10){
-        a1=0;b1=0;c1=0;d1=0;e1=0;
-        if(a==0){
-            if(palitos<2){
-                if(pthread_mutex_trylock(&lock)==0){
-                a++;
-                a1++;
+        if(a==0 && palitos<2){
+
+            if(pthread_mutex_trylock(&lock)==0){
+            a++;
+            //a1++;
+            palitos++;
+            //printf("usando a hilo: %ld   arr: %d \n",ids,arr);
+            pthread_mutex_unlock(&lock);
+            }
+
+        }
+        if(b==0 && palitos<2){
+
+            if(pthread_mutex_trylock(&lock1)==0){
+                b++;
+              //  b1++;
                 palitos++;
-                //printf("usando a hilo: %ld   arr: %d \n",ids,arr);
-                pthread_mutex_unlock(&lock);
-                }
+                //printf("usando b hilo: %ld   arr: %d \n",ids,arr);
+                pthread_mutex_unlock(&lock1);
             }
+
         }
-        if(b==0){
-            if(palitos<2){
-                if(pthread_mutex_trylock(&lock1)==0){
-                    b++;
-                    b1++;
-                    palitos++;
-                    //printf("usando b hilo: %ld   arr: %d \n",ids,arr);
-                    pthread_mutex_unlock(&lock1);
-                }
+        if(c==0 && palitos<2){
+
+            if(pthread_mutex_trylock(&lock2)==0){
+                c++;
+                //c1++;
+                palitos++;
+                //printf("usando c hilo: %ld   arr: %d \n",ids,arr);
+                pthread_mutex_unlock(&lock2);
             }
+
         }
-        if(c==0){
-            if(palitos<2){
-                if(pthread_mutex_trylock(&lock2)==0){
-                    c++;
-                    c1++;
-                    palitos++;
-                    //printf("usando c hilo: %ld   arr: %d \n",ids,arr);
-                    pthread_mutex_unlock(&lock2);
-                }
+        if(d==0 && palitos<2){
+            if(pthread_mutex_trylock(&lock3)==0){
+                d++;
+                //d1++;
+                palitos++;
+                //printf("usando d hilo: %ld   arr: %d \n",ids,arr);
+                pthread_mutex_unlock(&lock3);
             }
+
         }
-        if(d==0){
-            if(palitos<2){
-                if(pthread_mutex_trylock(&lock3)==0){
-                    d++;
-                    d1++;
-                    palitos++;
-                    //printf("usando d hilo: %ld   arr: %d \n",ids,arr);
-                    pthread_mutex_unlock(&lock3);
-                }
+        if(e==0 && palitos<2){
+
+            if(pthread_mutex_trylock(&lock4)==0){
+                e++;
+                //e1++;
+                palitos++;
+                //printf("usando e hilo: %ld   arr: %d \n",ids,arr);
+                pthread_mutex_unlock(&lock4);
             }
-        }
-        if(e==0){
-            if(palitos<2){
-                if(pthread_mutex_trylock(&lock4)==0){
-                    e++;
-                    e1++;
-                    palitos++;
-                    //printf("usando e hilo: %ld   arr: %d \n",ids,arr);
-                    pthread_mutex_unlock(&lock4);
-                }
-            }
+
         }
         if(palitos==2){
             printf("Filosofo %ld esta comiendo\n",ids);
-            mbool=0;
+            //mbool=0;
             arr++;
             sleep(1);
-        }else if(mbool=0){
+        }else{
             printf("Filosofo %ld esta hablando\n",ids);
-            mbool=1;
+            //mbool=1;
         }
+        pthread_barrier_wait(&our_barrier);
+        a=0;
+        b=0;
+        c=0;
+        d=0;
+        e=0;
+
+
+        /*
         pthread_mutex_trylock(&lock5);
         a=a-a1;
         b=b-b1;
@@ -91,6 +99,7 @@ void* filosofo(void *id){
         d=d-d1;
         e=e-e1;
         pthread_mutex_unlock(&lock5);
+        */
         palitos=0;
 
     }
@@ -130,6 +139,7 @@ int main(){
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
+    pthread_barrier_init(&our_barrier,NULL,5);
 
     for (id=0;id<NUM_HILOS;id++){
         rc=pthread_create(&arreglo_hilos[id],&attr,filosofo,(void*)id);
@@ -139,6 +149,7 @@ int main(){
     }
 
     pthread_attr_destroy(&attr);
+    pthread_barrier_destroy(&our_barrier);
 
     for (id=0;id<NUM_HILOS;id++)
     {
