0ac228a80478b12a8c6a14459faa04e95342a4b5
utec <jorgemayna@gmail.com>
2019-04-25 14:37:45 -0500

    OS lab_3.1 acabadoy envaido

--- Sistemas operativos/lab_3.1/filosofos.c
+++ Sistemas operativos/lab_3.1/filosofos.c
@@ -11,84 +11,99 @@ pthread_mutex_t lock3;
 pthread_mutex_t lock4;
 pthread_mutex_t lock5;
 pthread_barrier_t our_barrier;
+int cont=0;
+int acab=0;
 
 pthread_t arreglo_hilos[NUM_HILOS];
 void* filosofo(void *id){
     long ids=(long)id;
-    int mbool=0;
     int arr=0;
     int palitos=0;
-    int a1=0,b1=0,c1=0,d1=0,e1=0;
-    while(arr<10){
-        if(a==0 && palitos<2){
-
+    int i=0;
+    while(acab<5){
+        if(arr<20){
             if(pthread_mutex_trylock(&lock)==0){
-            a++;
-            //a1++;
-            palitos++;
-            //printf("usando a hilo: %ld   arr: %d \n",ids,arr);
-            pthread_mutex_unlock(&lock);
+                if(a==0 && palitos<2){
+                a++;
+                //a1++;
+                palitos++;
+                //printf("usando a hilo: %ld   arr: %d \n",ids,arr);
+
+                }
+                pthread_mutex_unlock(&lock);
             }
 
-        }
-        if(b==0 && palitos<2){
 
             if(pthread_mutex_trylock(&lock1)==0){
-                b++;
-              //  b1++;
-                palitos++;
-                //printf("usando b hilo: %ld   arr: %d \n",ids,arr);
+                if(b==0 && palitos<2){
+                    b++;
+                  //  b1++;
+                    palitos++;
+                    //printf("usando b hilo: %ld   arr: %d \n",ids,arr);
+
+                }
                 pthread_mutex_unlock(&lock1);
             }
 
-        }
-        if(c==0 && palitos<2){
 
             if(pthread_mutex_trylock(&lock2)==0){
-                c++;
-                //c1++;
-                palitos++;
-                //printf("usando c hilo: %ld   arr: %d \n",ids,arr);
+                if(c==0 && palitos<2){
+                    c++;
+                    //c1++;
+                    palitos++;
+                    //printf("usando c hilo: %ld   arr: %d \n",ids,arr);
+
+                }
                 pthread_mutex_unlock(&lock2);
             }
 
-        }
-        if(d==0 && palitos<2){
+
+
             if(pthread_mutex_trylock(&lock3)==0){
-                d++;
-                //d1++;
-                palitos++;
-                //printf("usando d hilo: %ld   arr: %d \n",ids,arr);
+                if(d==0 && palitos<2){
+                    d++;
+                    //d1++;
+                    palitos++;
+                    //printf("usando d hilo: %ld   arr: %d \n",ids,arr);
+
+                }
                 pthread_mutex_unlock(&lock3);
             }
 
-        }
-        if(e==0 && palitos<2){
-
             if(pthread_mutex_trylock(&lock4)==0){
-                e++;
-                //e1++;
-                palitos++;
-                //printf("usando e hilo: %ld   arr: %d \n",ids,arr);
+                if(e==0 && palitos<2){
+                    e++;
+                    //e1++;
+                    palitos++;
+                    //printf("usando e hilo: %ld   arr: %d \n",ids,arr);
+
+                }
                 pthread_mutex_unlock(&lock4);
             }
-
         }
+        i++;
         if(palitos==2){
-            printf("Filosofo %ld esta comiendo\n",ids);
+            printf("%d Filosofo %ld esta comiendo  su maki %d \n",i,ids,arr+1);
             //mbool=0;
             arr++;
-            sleep(1);
+            sleep(0.5);
+            palitos=0;
+            if(arr==20)acab++;
         }else{
-            printf("Filosofo %ld esta hablando\n",ids);
+            printf("%d Filosofo %ld esta hablando\n",i,ids);
             //mbool=1;
+            palitos=0;
         }
+        cont=i;
+
+
         pthread_barrier_wait(&our_barrier);
         a=0;
         b=0;
         c=0;
         d=0;
         e=0;
+        pthread_barrier_wait(&our_barrier);
 
 
         /*
@@ -100,7 +115,7 @@ void* filosofo(void *id){
         e=e-e1;
         pthread_mutex_unlock(&lock5);
         */
-        palitos=0;
+
 
     }
 
@@ -158,6 +173,7 @@ int main(){
             exit(-1);
         }
     }
+    printf("se demoro %d segundos \n",cont*4);
     pthread_mutex_destroy(&lock);
     pthread_mutex_destroy(&lock1);
     pthread_mutex_destroy(&lock2);
